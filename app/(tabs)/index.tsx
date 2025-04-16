44bae93e29ca9433340b2910fa95475dddc4af67
rajan <84809018+hatchedland@users.noreply.github.com>
2025-04-16 17:46:07 +0530

    ios deployment fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,31 @@
 import { View, Text } from 'react-native';
+import { useEffect } from 'react';
 import PropertiesScreen from './properties';
 import LandmarkDropdownFilters from '../components/LandmarkDropdownFilters';
 import LandingPage from '../components/Auth/LandingPage';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
 
 export default function TabOneScreen() {
+
+  useEffect(() => {
+    // Function to request permission and get the token
+    const getPushNotificationPermission = async () => {
+      // Request notification permissions
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (status === 'granted') {
+        const token = await Notifications.getExpoPushTokenAsync();
+        console.log('Expo Push Token:', token);
+        // Optionally send the token to your server to store for sending notifications
+      } else {
+        console.log('Notification permissions not granted');
+      }
+    };
+
+    getPushNotificationPermission();
+  }, []);
+  
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <LandingPage />
