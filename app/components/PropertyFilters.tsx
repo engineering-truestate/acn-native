eb90c4673f988794863d795e29f863bd57ca6cd5
yashbalyan08-IQOL <yash.balyan@truestate.in>
2025-04-17 04:44:19 +0530

    minor fix

--- app/components/PropertyFilters.tsx
+++ app/components/PropertyFilters.tsx
@@ -101,7 +101,7 @@ export default function PropertyFilters({
 
 const styles = StyleSheet.create({
   container: {
-    width: '95%',
+    width: '91%',
     borderBottomWidth: 0,
     borderBottomColor: '#E5E7EB',
     alignSelf: 'center',
