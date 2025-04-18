a05fb516b044b4ed58c004c156f1469e9b311bd0
jangirsamarthiqol <samarth@truestate.in>
2025-04-18 16:04:22 +0530

    added new svg

--- assets/icons/svg/HamburgerMenuIcon.tsx
+++ assets/icons/svg/HamburgerMenuIcon.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Svg, Rect, Path } from 'react-native-svg';
+import React from "react";
+import { Svg, Rect, Path } from "react-native-svg";
 
 interface HamburgerMenuIconProps {
   width?: number;
@@ -12,37 +12,37 @@ interface HamburgerMenuIconProps {
 const HamburgerMenuIcon: React.FC<HamburgerMenuIconProps> = ({
   width = 32,
   height = 33,
-  strokeColor = '#292D32',
-  rectStrokeColor = '#CCCBCB',
-  strokeWidth = 1.5
+  strokeColor = "#292D32",
+  rectStrokeColor = "#CCCBCB",
+  strokeWidth = 1.5,
 }) => {
   return (
     <Svg width={width} height={height} viewBox="0 0 32 33" fill="none">
-      <Rect 
-        x="0.5" 
-        y="1" 
-        width="31" 
-        height="31" 
-        rx="3.5" 
-        stroke={rectStrokeColor} 
+      <Rect
+        x="0.5"
+        y="1"
+        width="31"
+        height="31"
+        rx="3.5"
+        stroke={rectStrokeColor}
       />
-      <Path 
-        d="M7 11.5H25" 
-        stroke={strokeColor} 
-        strokeWidth={strokeWidth} 
-        strokeLinecap="round" 
+      <Path
+        d="M7 11.5H25"
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
       />
-      <Path 
-        d="M7 16.5H25" 
-        stroke={strokeColor} 
-        strokeWidth={strokeWidth} 
-        strokeLinecap="round" 
+      <Path
+        d="M7 16.5H25"
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
       />
-      <Path 
-        d="M7 21.5H25" 
-        stroke={strokeColor} 
-        strokeWidth={strokeWidth} 
-        strokeLinecap="round" 
+      <Path
+        d="M7 21.5H25"
+        stroke={strokeColor}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
       />
     </Svg>
   );
